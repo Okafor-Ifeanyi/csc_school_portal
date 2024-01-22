ce277ad05efc28648dff991fe42a854b9ede870b
Okafor Ifeanyi <zeusifeanyi058@gmail.com>
2024-01-22 09:52:58 +0100

    feat(test-user): added tests on auth and users

--- src/__tests__/user.test.ts
+++ src/__tests__/user.test.ts
@@ -1,15 +1,16 @@
 import supertest from "supertest";
 import { MongoMemoryServer } from "mongodb-memory-server";
 import { createServer } from "../configs/server.config";
-import { loginAdmin_wrong } from "./payload";
+import { loginAdmin_wrong, registerAdmin, registerStudent } from "./payload";
 import mongoose from "mongoose";
 
 const app = createServer();
 
 describe("User", () => {
+  jest.setTimeout(30000)
+
   beforeAll(async () => {
     const mongoServer = await MongoMemoryServer.create();
-    console.log(mongoServer.getUri());
     await mongoose.connect(mongoServer.getUri());
   });
 
@@ -35,11 +36,48 @@ describe("User", () => {
           .send(loginAdmin_wrong);
 
         expect(result.status).toBe(401);
-        // expect(result.body).toMatchObject({ message: "Could not find admin" });
+        expect(result.body).toMatchObject({ message: `username ${loginAdmin_wrong.username} not found.` });
       });
     });
+    
+    describe("Get the registered user", () => {
+      it("returns logged in as an Admin", async () => {
+        await supertest(app).post(
+          "/api/admins/register").send(registerAdmin)
 
-    // describe("Get the registered user", () => {
-    // })
+
+        const result = await supertest(app)
+          .post(`/api/auth/login`).send({
+            username: registerAdmin.email, 
+            password: registerAdmin.password
+          });
+        
+        expect(result.status).toBe(201);
+        expect(result.body).toMatchObject({ message: `Logged in as : admin` });
+        expect(result.body.user).toMatchObject({ 
+          username: registerAdmin.email,
+          type: "admin",
+        });
+      })
+
+      it("returns logged in as a Student", async () => {
+        await supertest(app).post(
+          "/api/students/register").send(registerStudent)
+
+        const result = await supertest(app)
+          .post(`/api/auth/login`).send({
+            username: registerStudent.reg_number, 
+            password: registerStudent.password
+          });
+        
+        expect(result.status).toBe(201);
+        expect(result.body).toMatchObject({ message: `Logged in as : student` });
+        expect(result.body.user).toMatchObject({ 
+          username: registerStudent.reg_number,
+          type: "student",
+        });
+      })
+    })
+
   });
 });

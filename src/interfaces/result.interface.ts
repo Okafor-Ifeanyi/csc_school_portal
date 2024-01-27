71aec2ea05479353288624735854bd5a94991ee5
Okafor Ifeanyi <zeusifeanyi058@gmail.com>
2024-01-27 22:32:29 +0100

    feat(result-model): added result interface model from ERM

---
+++ src/interfaces/result.interface.ts
@@ -0,0 +1,9 @@
+import { ObjectId } from "mongoose";
+
+export interface IResult {
+    session_id: ObjectId;
+    student_id: ObjectId;
+    gpa: number;
+    tnu: number;
+    isCurrent: boolean
+}
---
+++ src/models/result.model.ts
@@ -0,0 +1,45 @@
+import { Schema, model, Model, Document, Types } from "mongoose";
+import { IResult } from "../interfaces/result.interface";
+
+export interface ResultDocument extends Document, IResult {}
+
+type ResultModel = Model<ResultDocument>;
+
+const ResultSchema = new Schema<IResult, ResultModel>(
+  {
+    session_id: {
+      type: Types.ObjectId,
+      ref: "Session",
+      required: true,
+    },
+    student_id: {
+      type: Types.ObjectId,
+      ref: "Student",
+      required: true,
+    },
+    gpa: {
+      type: Number,
+      default: 0,
+    },
+    tnu: {
+      type: Number,
+      default: 0,
+    },
+    isCurrent: {
+      type: Boolean,
+      default: true
+    }
+  },
+  {
+    timestamps: true,
+  },
+);
+
+ResultSchema.index(
+  { session_id: 1, course_id: 1, student_id: 1 },
+  { unique: true },
+);
+
+const Result = model<IResult, ResultModel>( "Result", ResultSchema, );
+
+export default Result;
